f894de524c5e053741de975dec7ba9eb7b637eb3
quarkusio <no-reply@quarkus.io>
2026-07-26 13:17:37 +0000

    Initial commit

---
+++ src/test/java/net/herhoffer/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package net.herhoffer;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
